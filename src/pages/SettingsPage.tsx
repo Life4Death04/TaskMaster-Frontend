5ca0493c1fc783d44dc32d91047cd989abf6db13
Life4Death <santiagodrm@gmail.com>
2026-01-12 13:27:08 +0100

    feat(ui - settings): add settings page/view (no logic)

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,9 @@
+import { SettingsContainer } from '../containers/SettingsContainer';
+
+/**
+ * Settings Page
+ * Main page component for the Account Settings feature
+ */
+export const SettingsPage = () => {
+    return <SettingsContainer />;
+};

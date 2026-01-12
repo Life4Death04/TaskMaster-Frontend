67c605f83bb4f904f3aebbf559979e4d2f4e26de
Life4Death <santiagodrm@gmail.com>
2026-01-12 12:35:23 +0100

    feat(ui - My Lists): Add My Lists UI feature (no logic)

---
+++ src/pages/ListsPage.tsx
@@ -0,0 +1,9 @@
+import { ListsContainer } from '../containers/ListsContainer';
+
+/**
+ * Lists Page
+ * Main page component for the My Lists feature
+ */
+export const ListsPage = () => {
+    return <ListsContainer />;
+};
